846ad781b77f7f92860a826da9266aa777a9b794
AsifProg <mirzaasifali209@gmail.com>
2025-02-14 19:55:49 +0300

    Adjusted the primary colors

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
     # path("__reload__/", include("django_browser_reload.urls")),
 ]
 
-# if settings.DEBUG:
-#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
